556fe2edba9eecdf5345179bc2b958c02a635626
KyleXu <kyle_x@foxmail.com>
2024-01-21 12:37:36 +0800

    add new theme

--- bin/themes.sh
+++ bin/themes.sh
@@ -6,3 +6,4 @@ cp -Rf "./node_modules/headline" ./content/themes/headline;
 cp -Rf "./node_modules/ruby" ./content/themes/ruby;
 cp -Rf "./node_modules/ease" ./content/themes/ease;
 cp -Rf "./node_modules/wave" ./content/themes/wave;
+cp -Rf "./node_modules/dawn" ./content/themes/dawn;

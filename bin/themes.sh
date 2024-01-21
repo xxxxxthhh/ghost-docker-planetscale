9682444249ca3c65bd33ac836176c5f8f5b8aeeb
KyleXu <kyle_x@foxmail.com>
2024-01-21 14:19:05 +0800

    test how to set theme

--- bin/themes.sh
+++ bin/themes.sh
@@ -1,9 +1,9 @@
 #/bin/sh
 
+cp -Rf "./node_modules/dawn" ./content/themes/dawn;
 cp -Rf "./node_modules/casper" ./content/themes/capser;
 cp -Rf "./node_modules/lyra" ./content/themes/lyra;
 cp -Rf "./node_modules/headline" ./content/themes/headline;
 cp -Rf "./node_modules/ruby" ./content/themes/ruby;
 cp -Rf "./node_modules/ease" ./content/themes/ease;
 cp -Rf "./node_modules/wave" ./content/themes/wave;
-cp -Rf "./node_modules/dawn" ./content/themes/dawn;
